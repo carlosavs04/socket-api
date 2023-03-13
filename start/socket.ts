f31dde9901b138823b5a262fcdd2e6dfe1b55e35
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-03-13 17:24:27 -0600

    escuchando

--- start/socket.ts
+++ start/socket.ts
@@ -6,10 +6,9 @@ Ws.boot()
  * Listen for incoming socket connections
  */
 Ws.io.on('connection', (socket) => {
-  socket.emit('news','Servidor escuhando')
+  socket.emit('news','Servidor escuchando')
 
   socket.on('my other event', (data) => {
     console.log(data)
   })
-
 })
