7987ea683a5d53a26b6a28bce28df9a7987a2f9b
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-03-14 23:24:31 -0600

    Ahora sí eventSource

--- app/Controllers/Http/ConsolasController.ts
+++ app/Controllers/Http/ConsolasController.ts
@@ -2,34 +2,34 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Consola from 'App/Models/Consola'
 import Event from '@ioc:Adonis/Core/Event'
-
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class ConsolasController {
-  public async createConsola({request,response}:HttpContextContract){
+  public async createConsola({request, response}:HttpContextContract) {
     await request.validate({
       schema: schema.create({
-          nombre: schema.string([
-              rules.maxLength(20),
-          ])
+        nombre: schema.string([
+          rules.maxLength(20),
+        ])
       }),
       messages: {
-          required: 'El campo {{ field }} es obligatorio.',
-          maxLength: 'El campo {{ field }} debe tener un máximo de {{ options.maxLength }} caracteres.',
-          string: 'El campo {{ field }} debe ser una cadena de caracteres.',
+        required: 'El campo {{ field }} es obligatorio.',
+        maxLength: 'El campo {{ field }} debe tener un máximo de {{ options.maxLength }} caracteres.',
+        string: 'El campo {{ field }} debe ser una cadena de caracteres.',
       }
-  })
+    })
 
-    var consola = await Consola.create({
-        nombre: request.input('nombre'),
+    const consola = await Consola.create({
+      nombre: request.input('nombre'),
     })
 
     Event.emit('new:consola', { id: consola.id })
 
     return response.created({
-        'status': 201,
-        'mensaje': 'Los datos fueron almacenados correctamente.',
-        'error': [],
-        'data': consola
+      'status': 201,
+      'mensaje': 'Los datos fueron almacenados correctamente.',
+      'error': [],
+      'data': consola
     })
   }
 
@@ -37,5 +37,17 @@ export default class ConsolasController {
     const consolas = await Consola.query().select('*')
 
     return consolas
+  }
+
+  public async eventSource({ response }: HttpContextContract) {
+    response.header('Content-Type', 'text/event-stream')
+    response.header('Cache-Control', 'no-cache')
+    response.header('Connection', 'keep-alive')
+
+    if (Env.get('EVENT_LISTENER') === 'true') {
+      const consolas = await Consola.query().select('*')
+      response.send(`event: new:consola\ndata: ${JSON.stringify(consolas)}\n\n`)
+      Env.set('EVENT_LISTENER', 'false')
+    }
   }
 }
--- app/Listeners/Consola.ts
+++ app/Listeners/Consola.ts
@@ -1,8 +1,9 @@
 import type { EventsList } from '@ioc:Adonis/Core/Event'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class Consola {
   public async onNewConsola(consola: EventsList['new:consola']) {
     console.log('New consola created with id: ' + consola.id)
+    Env.set('EVENT_LISTENER', 'true')
   }
-
 }
